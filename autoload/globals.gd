e4dc91dc1f9cea60b19645b37dc52bead8b8d008
jprn <cafeore56@gmail.com>
2025-11-18 23:34:33 +0900

    5

--- autoload/globals.gd
+++ autoload/globals.gd
@@ -1,1 +1,7 @@
 extends Node
+## Globals
+
+var gate_id: int = 0
+
+var money: int = 0
+var exp: int = 0
--- autoload/save_loader.gd
+++ autoload/save_loader.gd
@@ -7,7 +7,7 @@ const FILE_PATH: String = "user://gamedata.cfg"
 
 
 func _ready():
-	if !FileAccess.file_exists(FILE_PATH):
+	if !FileAccess.file_exists(FILE_PATH):	# データが無い場合は初期値を入力
 		config.set_value("data", "gate_id", 0)
 		
 		config.save_encrypted_pass(FILE_PATH, "mai")
@@ -17,7 +17,9 @@ func _ready():
 
 
 func save_game():
-	config.set_value("data", "gate_id", "変数など")
+	config.set_value("data", "gate_id", Globals.gate_id)
+	config.set_value("data", "money", Globals.money)
+	config.set_value("data", "exp", Globals.exp)
 	
 	config.save_encrypted_pass(FILE_PATH, "mai")
 
@@ -27,19 +29,7 @@ func load_game():
 		for key in config.get_section_keys(section):
 			if key == "gate_id":
 				Globals.gate_id = config.get_value(section, key)
-
-
-#func save_player(key: String, value):
-	#config.set_value("player", key, value)
-	#config.save_encrypted_pass(FILE_PATH, "mai")
-#
-#func load_player():
-	#var dic = {}
-	#for key in config.get_section_keys("player"):
-		#dic[key] = config.get_value("player", key)
-	#
-	#return dic
-
-
-func all_reset():
-		Globals.gate_id = 0
+			elif key == "money":
+				Globals.money = config.get_value(section, key)
+			elif key == "exp":
+				Globals.exp = config.get_value(section, key)
